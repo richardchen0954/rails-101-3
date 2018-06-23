67b2971dbda57231b82f15030132fb5eff0a5e76
西影行 <richardchen0954@gmail.com>
2018-06-23 17:16:01 +0800

    group_relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :groups
   has_many :posts
+  has_many :group_relationships
+  has_many :paticipated_groups, :through => :group_relationships, :source => :group
+
+  def is_member_of?(group)
+    paticipated_groups.include?(group)
+  end
+
+  def join!(group)
+    paticipated_groups << group
+  end
+  def quit!(group)
+    paticipated_groups.delete(group)
+  end
 end
