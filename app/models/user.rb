c9d8c8da1d6fb31c1399cdb7753392e5ec16805c
西影行 <richardchen0954@gmail.com>
2018-06-24 00:04:25 +0800

    dropdown_menu(My Group & My Posts)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,16 @@ class User < ApplicationRecord
   has_many :groups
   has_many :posts
   has_many :group_relationships
-  has_many :paticipated_groups, :through => :group_relationships, :source => :group
+  has_many :participated_groups, :through => :group_relationships, :source => :group
 
   def is_member_of?(group)
-    paticipated_groups.include?(group)
+    participated_groups.include?(group)
   end
 
   def join!(group)
-    paticipated_groups << group
+    participated_groups << group
   end
   def quit!(group)
-    paticipated_groups.delete(group)
+    participated_groups.delete(group)
   end
 end
